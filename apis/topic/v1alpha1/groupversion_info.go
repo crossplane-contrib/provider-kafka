18dfaf26536177803a07a7afca9926353297973f
Hasan Turken <turkenh@gmail.com>
2021-10-12 20:50:10 +0300

    Implement Create and Delete methods
    Signed-off-by: Hasan Turken <turkenh@gmail.com>
    Signed-off-by: jograca <jon.graca@gmail.com>

--- apis/topic/v1alpha1/groupversion_info.go
+++ apis/topic/v1alpha1/groupversion_info.go
@@ -16,7 +16,7 @@ limitations under the License.
 
 // Package v1alpha1 contains the v1alpha1 group Sample resources of the Template provider.
 // +kubebuilder:object:generate=true
-// +groupName=topic.template.crossplane.io
+// +groupName=topic.kafka.crossplane.io
 // +versionName=v1alpha1
 package v1alpha1
 
@@ -27,7 +27,7 @@ import (
 
 // Package type metadata.
 const (
-	Group   = "topic.template.crossplane.io"
+	Group   = "topic.kafka.crossplane.io"
 	Version = "v1alpha1"
 )
 
--- apis/v1alpha1/groupversion_info.go
+++ apis/v1alpha1/groupversion_info.go
@@ -16,7 +16,7 @@ limitations under the License.
 
 // Package v1alpha1 contains the core resources of the Template provider.
 // +kubebuilder:object:generate=true
-// +groupName=template.crossplane.io
+// +groupName=kafka.crossplane.io
 // +versionName=v1alpha1
 package v1alpha1
 
@@ -27,7 +27,7 @@ import (
 
 // Package type metadata.
 const (
-	Group   = "template.crossplane.io"
+	Group   = "kafka.crossplane.io"
 	Version = "v1alpha1"
 )
 
