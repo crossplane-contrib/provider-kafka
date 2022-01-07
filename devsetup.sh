3cbbe0df36b764af5cbbc2b56497cc6f712f1c21
Mallory Quaintance <mallory.quaintance@libertymutual.com>
2022-01-07 14:28:47 -0500

    Adding simple script for easy install and password configs

---
+++ devsetup.sh
@@ -0,0 +1,10 @@
+set -x
+echo "This script only installs the kafka development cluster via helm, and sets up the credentials.  You will need to follow the rest of the instructions to install helm, your kube cluster, etc.)"
+helm repo add bitnami https://charts.bitnami.com/bitnami;
+kubectl create ns kafka-cluster;
+helm upgrade --install kafka-dev -n kafka-cluster bitnami/kafka --set auth.clientProtocol=sasl --set deleteTopicEnable=true --set autoCreateTopicsEnable=false --wait;
+CREDS=$(kubectl -n kafka-cluster exec kafka-dev-0 -- cat /opt/bitnami/kafka/config/kafka_jaas.conf | grep password | awk -F\" '{print $2}');
+sed 's/<your-password>/'"$CREDS"'/g' devcreds.json > kc.json;
+sed 's/<your-password>/'"$CREDS"'/g' devkafcliconfig > ~/.kaf/config;
+kubectl -n crossplane-system create secret generic kafka-creds --from-file=credentials=kc.json;
+kaf config use-cluster local
