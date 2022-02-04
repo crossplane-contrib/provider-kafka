0209bdfcfdc72a1b25cc7b0273d853588f80f4f2
Ken Lavoie <ken.lavoie@libertymutual.com>
2022-02-04 08:55:01 -0500

    adding first version of delete and duplicate topic test cases

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/google/go-cmp v0.5.6
 	github.com/pkg/errors v0.9.1
 	github.com/ramya-rao-a/go-outline v0.0.0-20210608161538-9736a4bde949 // indirect
+	github.com/stretchr/objx v0.3.0 // indirect
 	github.com/stretchr/testify v1.7.0 // indirect
 	github.com/twmb/franz-go v1.2.3-0.20211102021212-9a7f9860bbb6
 	github.com/twmb/franz-go/pkg/kadm v0.0.0-20211102021212-9a7f9860bbb6
--- internal/clients/kafka/topic/topic_test.go
+++ internal/clients/kafka/topic/topic_test.go
@@ -1,11 +1,220 @@
 package topic
 
 import (
+	"context"
 	"fmt"
 	"github.com/crossplane-contrib/provider-kafka/apis/topic/v1alpha1"
+	"github.com/crossplane-contrib/provider-kafka/internal/clients/kafka"
 	"github.com/google/go-cmp/cmp"
+	"github.com/twmb/franz-go/pkg/kadm"
 	"testing"
 )
+
+func TestDelete(t *testing.T) {
+
+	var dataTesting = []byte(`{
+  "brokers": [
+    "kafka-dev-0.kafka-dev-headless:9092"
+  ],
+  "sasl": {
+    "mechanism": "PLAIN",
+    "username": "user",
+    "password": ""
+  }
+}`)
+
+	newAc, _ := kafka.NewAdminClient(dataTesting)
+
+	type args struct {
+		ctx    context.Context
+		client *kadm.Client
+		name   string
+	}
+	cases := map[string]struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		"DeleteTopicZero": {
+			name: "DeleteTopicZero",
+			args: args{
+				ctx:    context.Background(),
+				client: newAc,
+				name:   "create0",
+			},
+			wantErr: true,
+		},
+		"DeleteTopicOne": {
+			name: "DeleteTopicOne",
+			args: args{
+				ctx:    context.Background(),
+				client: newAc,
+				name:   "create1",
+			},
+			wantErr: false,
+		},
+		"DeleteTopicTwo": {
+			name: "DeleteTopicTwo",
+			args: args{
+				ctx:    context.Background(),
+				client: newAc,
+				name:   "create2",
+			},
+			wantErr: false,
+		},
+		"DeleteTopicThree": {
+			name: "DeleteTopicThree",
+			args: args{
+				ctx:    context.Background(),
+				client: newAc,
+				name:   "create3",
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range cases {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := Delete(tt.args.ctx, tt.args.client, tt.args.name); (err != nil) != tt.wantErr {
+				t.Errorf("Delete() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestCreate(t *testing.T) {
+
+	var dataTesting = []byte(`{
+		"brokers": [ "kafka-dev-0.kafka-dev-headless:9092"],
+	"sasl": {
+	"mechanism": "PLAIN",
+	"username": "user",
+	"password": ""
+	}
+	}`)
+
+	newAc, _ := kafka.NewAdminClient(dataTesting)
+
+	type args struct {
+		ctx    context.Context
+		client *kadm.Client
+		topic  *Topic
+	}
+	{
+		cases := map[string]struct {
+			name    string
+			args    args
+			wantErr bool
+		}{
+			"CreateTopicOne": {
+				name: "CreateTopicOne",
+				args: args{
+					ctx:    context.Background(),
+					client: newAc,
+					topic: &Topic{
+						Name:              "testTopic-1",
+						ReplicationFactor: 1,
+						Partitions:        1,
+						Config:            nil,
+					},
+				},
+				wantErr: false,
+			},
+
+			"CreateTopicTwo": {
+				name: "CreateTopicTwo",
+				args: args{
+					ctx:    context.Background(),
+					client: newAc,
+					topic: &Topic{
+						Name:              "testTopic-2",
+						ReplicationFactor: 1,
+						Partitions:        1,
+						Config:            nil,
+					},
+				},
+				wantErr: false,
+			},
+
+			"CreateTopicThree": {
+				name: "CreateTopicThree",
+				args: args{
+					ctx:    context.Background(),
+					client: newAc,
+					topic: &Topic{
+						Name:              "testTopic-3",
+						ReplicationFactor: 1,
+						Partitions:        1,
+						Config:            nil,
+					},
+				},
+				wantErr: false,
+			},
+		}
+
+		// TODO: Add test cases.
+
+		for _, tt := range cases {
+			t.Run(tt.name, func(t *testing.T) {
+				if err := Create(tt.args.ctx, tt.args.client, tt.args.topic); (err != nil) != tt.wantErr {
+					t.Errorf("Create() error = %v, wantErr %v", err, tt.wantErr)
+				}
+			})
+		}
+	}
+}
+
+func TestGet(t *testing.T) {
+
+	var dataTesting = []byte(`{
+		"brokers": [ "kafka-dev-0.kafka-dev-headless:9092"],
+	"sasl": {
+	"mechanism": "PLAIN",
+	"username": "user",
+	"password": ""
+	}
+	}`)
+
+	newAc, _ := kafka.NewAdminClient(dataTesting)
+
+	type args struct {
+		ctx    context.Context
+		client *kadm.Client
+		name   string
+	}
+	cases := map[string]struct {
+		name    string
+		args    args
+		want    *Topic
+		wantErr bool
+	}{
+		"GetTopicWorked": {
+			name: "GetTopicWorked",
+			args: args{
+				ctx:    context.Background(),
+				client: newAc,
+				name:   "create1",
+			},
+			want: &Topic{
+				Name:              "create1",
+				ReplicationFactor: 1,
+				Partitions:        1,
+				Config:            nil,
+			},
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range cases {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := Get(tt.args.ctx, tt.args.client, tt.args.name)
+			fmt.Println(err)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Get() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+		})
+	}
+}
 
 func TestGenerate(t *testing.T) {
 	type args struct {
@@ -161,5 +370,89 @@ func TestIsUpToDate(t *testing.T) {
 			}
 
 		})
+	}
+}
+
+func TestCreateDuplicateTopic(t *testing.T) {
+
+	var dataTesting = []byte(`{
+		"brokers": [ "kafka-dev-0.kafka-dev-headless:9092"],
+	"sasl": {
+	"mechanism": "PLAIN",
+	"username": "user",
+	"password": ""
+	}
+	}`)
+
+	newAc, _ := kafka.NewAdminClient(dataTesting)
+
+	fmt.Printf("------Checking duplicate topic creation logic------")
+
+	type args struct {
+		ctx    context.Context
+		client *kadm.Client
+		topic  *Topic
+	}
+	{
+		cases := map[string]struct {
+			name    string
+			args    args
+			wantErr bool
+		}{
+			"CreateTopicOneExists": {
+				name: "CreateTopicOneExists \n",
+				args: args{
+					ctx:    context.Background(),
+					client: newAc,
+					topic: &Topic{
+						Name:              "create1",
+						ReplicationFactor: 1,
+						Partitions:        1,
+						Config:            nil,
+					},
+				},
+				wantErr: true,
+			},
+
+			"CreateTopicTwoExists": {
+				name: "CreateTopicTwoExists",
+				args: args{
+					ctx:    context.Background(),
+					client: newAc,
+					topic: &Topic{
+						Name:              "create2",
+						ReplicationFactor: 1,
+						Partitions:        1,
+						Config:            nil,
+					},
+				},
+				wantErr: true,
+			},
+
+			"CreateTopicThreeExists": {
+				name: "CreateTopicThreeExists",
+				args: args{
+					ctx:    context.Background(),
+					client: newAc,
+					topic: &Topic{
+						Name:              "create3",
+						ReplicationFactor: 1,
+						Partitions:        1,
+						Config:            nil,
+					},
+				},
+				wantErr: true,
+			},
+		}
+
+		// TODO: Add test cases.
+
+		for _, tt := range cases {
+			t.Run(tt.name, func(t *testing.T) {
+				if err := Create(tt.args.ctx, tt.args.client, tt.args.topic); (err != nil) != tt.wantErr {
+					t.Errorf("Create() error = %v, wantErr %v", err, tt.wantErr)
+				}
+			})
+		}
 	}
 }
