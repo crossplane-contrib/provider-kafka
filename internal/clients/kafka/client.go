16549cb0bdd87a1819324cecd72df1e0e9ea697c
mdhwk <martin@xkmf.se>
2022-05-07 17:41:42 +0200

    update client to support aws iam
    Signed-off-by: mdhwk <martin@xkmf.se>

--- internal/clients/kafka/client.go
+++ internal/clients/kafka/client.go
@@ -1,11 +1,15 @@
 package kafka
 
 import (
+	"context"
 	"encoding/json"
-
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sts"
 	"github.com/pkg/errors"
 	"github.com/twmb/franz-go/pkg/kadm"
 	"github.com/twmb/franz-go/pkg/kgo"
+	"github.com/twmb/franz-go/pkg/sasl"
+	kaws "github.com/twmb/franz-go/pkg/sasl/aws"
 	"github.com/twmb/franz-go/pkg/sasl/plain"
 )
 
@@ -22,13 +26,11 @@ func NewAdminClient(data []byte) (*kadm.Client, error) {
 	}
 
 	if kc.SASL != nil {
-		if kc.SASL.Mechanism != "PLAIN" {
-			return nil, errors.Errorf("SASL mechanisms %q not supported, only %q supported for now.", kc.SASL.Username, "PLAIN")
+		s, err := authenticateWithSASL(kc.SASL)
+		if err != nil {
+			return nil, errors.Wrap(err, "unable to perform authentication")
 		}
-		opts = append(opts, kgo.SASL(plain.Auth{
-			User: kc.SASL.Username,
-			Pass: kc.SASL.Password,
-		}.AsMechanism()))
+		opts = append(opts, kgo.SASL(s))
 	}
 
 	c, err := kgo.NewClient(opts...)
@@ -36,4 +38,63 @@ func NewAdminClient(data []byte) (*kadm.Client, error) {
 		return nil, err
 	}
 	return kadm.NewClient(c), nil
+}
+
+func authenticateWithSASL(sasl *SASL) (sasl.Mechanism, error) {
+	switch sasl.Mechanism {
+	case "PLAIN":
+		return plain.Auth{
+			User: sasl.Username,
+			Pass: sasl.Password,
+		}.AsMechanism(), nil
+	case "AWS_MSK_IAM":
+		return awsMskIamAuthentication(sasl)
+	default:
+		return nil, errors.Errorf("SASL mechanisms %q not supported, only %q supported for now.", sasl.Mechanism, "PLAIN & AWS_MSK_IAM")
+	}
+}
+
+func awsMskIamAuthentication(sasl *SASL) (sasl.Mechanism, error) {
+	s, err := session.NewSession()
+	if err != nil {
+		return nil, err
+	}
+
+	if sasl.RoleArn != "" {
+		return assumeAwsRole(s, sasl.RoleArn)
+	}
+
+	val, err := s.Config.Credentials.GetWithContext(context.TODO())
+	if err != nil {
+		return nil, err
+	}
+
+	a := kaws.Auth{
+		AccessKey:    val.AccessKeyID,
+		SecretKey:    val.SecretAccessKey,
+		SessionToken: val.SessionToken,
+	}
+
+	return a.AsManagedStreamingIAMMechanism(), nil
+}
+
+func assumeAwsRole(s *session.Session, roleArn string) (sasl.Mechanism, error) {
+	sc := sts.New(s)
+	sn := "crossplane_provider_kafka_session"
+
+	res, err := sc.AssumeRole(&sts.AssumeRoleInput{
+		RoleArn:         &roleArn,
+		RoleSessionName: &sn,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	a := kaws.Auth{
+		AccessKey:    *res.Credentials.AccessKeyId,
+		SecretKey:    *res.Credentials.SecretAccessKey,
+		SessionToken: *res.Credentials.SessionToken,
+	}
+
+	return a.AsManagedStreamingIAMMechanism(), nil
 }
