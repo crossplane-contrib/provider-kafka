31671867ddb3757e22e76043dd50d54b632e61c1
Renzo Toma <renzo.toma@gmail.com>
2022-07-18 12:09:19 +0200

    feat: add mtls support (resolves #37)
    Signed-off-by: Renzo Toma <renzo.toma@gmail.com>

--- internal/clients/kafka/client.go
+++ internal/clients/kafka/client.go
@@ -1,6 +1,7 @@
 package kafka
 
 import (
+	"context"
 	"crypto/tls"
 	"encoding/json"
 	"os"
@@ -12,14 +13,27 @@ import (
 	"github.com/twmb/franz-go/pkg/sasl"
 	"github.com/twmb/franz-go/pkg/sasl/plain"
 	"github.com/twmb/franz-go/pkg/sasl/scram"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+const (
+	// default Secret field names for TLS certificates, like managed by cert-manager
+	defaultClientCertificateKeyField  = "tls.key"
+	defaultClientCertificateCertField = "tls.crt"
+
+	errCannotParse                    = "cannot parse credentials"
+	errMissingClientCertSecretRefKeys = "missing client cert ref secret name or namespace"
+	errCannotReadClientCertSecret     = "cannot read client cert secret"
 )
 
 // NewAdminClient creates a new AdminClient with supplied credentials
-func NewAdminClient(data []byte) (*kadm.Client, error) {
+func NewAdminClient(ctx context.Context, data []byte, kube client.Client) (*kadm.Client, error) {
 	kc := Config{}
 
 	if err := json.Unmarshal(data, &kc); err != nil {
-		return nil, errors.Wrap(err, "cannot parse credentials")
+		return nil, errors.Wrap(err, errCannotParse)
 	}
 
 	opts := []kgo.Opt{
@@ -48,6 +62,25 @@ func NewAdminClient(data []byte) (*kadm.Client, error) {
 
 	if kc.TLS != nil {
 		tc := new(tls.Config)
+		tc.InsecureSkipVerify = kc.TLS.InsecureSkipVerify
+
+		if sr := kc.TLS.ClientCertificateSecretRef; sr != nil {
+			if sr.Name == "" || sr.Namespace == "" {
+				return nil, errors.New(errMissingClientCertSecretRefKeys)
+			}
+			secret := &corev1.Secret{}
+			if err := kube.Get(ctx, types.NamespacedName{Namespace: sr.Namespace, Name: sr.Name}, secret); err != nil {
+				return nil, errors.Wrap(err, errCannotReadClientCertSecret)
+			}
+			kf := valueOrDefault(sr.KeyField, defaultClientCertificateKeyField)
+			cf := valueOrDefault(sr.CertField, defaultClientCertificateCertField)
+			kp, err := tls.X509KeyPair(secret.Data[cf], secret.Data[kf])
+			if err != nil {
+				return nil, errors.Wrapf(err, "Invalid key pair, using fields %q/%q from secret %q in namespace %q",
+					cf, kf, sr.Name, sr.Namespace)
+			}
+			tc.Certificates = append(tc.Certificates, kp)
+		}
 		opts = append(opts, kgo.DialTLSConfig(tc))
 	}
 
@@ -56,4 +89,12 @@ func NewAdminClient(data []byte) (*kadm.Client, error) {
 		return nil, err
 	}
 	return kadm.NewClient(c), nil
+}
+
+// Helper method to return default if value string is empty.
+func valueOrDefault(value, defaultValue string) string {
+	if value != "" {
+		return value
+	}
+	return defaultValue
 }
